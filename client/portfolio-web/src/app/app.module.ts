3768ea99ba424adfb80045319e81d195c18e36e1
ramsesrodriguez <rodriguezgarciaramses@gmail.com>
2020-08-15 18:49:30 +0200

    Template main page

--- client/portfolio-web/src/app/app.module.ts
+++ client/portfolio-web/src/app/app.module.ts
@@ -3,16 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/site/home/home.component';
+import { AboutComponent } from './components/site/about/about.component';
+import { ContactComponent } from './components/site/contact/contact.component';
+import { PortfolioComponent } from './components/site/portfolio/portfolio.component';
+import { FooterComponent } from './components/site/footer/footer.component';
+import { CtaComponent } from './components/site/cta/cta.component';
+import { HeaderComponent } from './components/site/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    PortfolioComponent,
+    FooterComponent,
+    CtaComponent,
+    HeaderComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
